31f08d3cc283b2d8cd1a4c05684d66ae25bc96ef
ekdls <ekdls278@gmail.com>
2023-05-24 17:12:27 +0900

    main update

--- app/src/main/java/com/AjouManagement/app/MainActivity.java
+++ app/src/main/java/com/AjouManagement/app/MainActivity.java
@@ -9,8 +9,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.StackView;
 
@@ -23,13 +26,27 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DragListener.Listener {
     private ActivityMainBinding binding;
+
     private ArrayList<MainData> arrayList;
+    private ArrayList<MainData> addArrayList;
+
     private MainAdapter mainAdapter;
+    private MainAdapter addAdapter;
+
     private RecyclerView recyclerView;
+    private RecyclerView addRecyclerView;
+
     private LinearLayoutManager linearLayoutManager;
+    private LinearLayoutManager addLayoutManager;
+
     private ItemTouchHelper mItemTouchHelper;
+    private ItemTouchHelper addItemTouchHelper;
+
+    private FallingBall fallingBall;
+
+    private boolean isVisible = true;
 
 
     @Override
@@ -37,8 +54,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        binding.rvAddRoutine.setVisibility(View.GONE);
         setContentView(view);
-
+        initMainRecyclerView();
+        initAddRecyclerView();
         binding.btnAddRoutine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         binding.btnCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,13 +72,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isVisible){
+                    showAddList();
+                    binding.btnAddRoutine.setVisibility(View.GONE);
+                }else{
+                    hideAddList();
+                    binding.btnAddRoutine.setVisibility(View.VISIBLE);
+                }
 
+            }
+        });
+
+    }
+    private void initMainRecyclerView(){
+        //Main RecyclerView 설정
         recyclerView = binding.rv;
 
-
         linearLayoutManager =  new LinearLayoutManager(this);
-        //linearLayoutManager.setReverseLayout(true);
         linearLayoutManager.setStackFromEnd(true);
+
 
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.addItemDecoration(new MainItemDecoration(-150));
@@ -70,20 +103,70 @@ public class MainActivity extends AppCompatActivity {
             MainData testData = new MainData("TestData" +i);
             arrayList.add(testData);
         }
-
         Collections.reverse(arrayList);
 
-
-        mainAdapter = new MainAdapter(arrayList);
+        //Main RecyclerView Adapter설정
+        mainAdapter = new MainAdapter(arrayList, this);
         recyclerView.setAdapter(mainAdapter);
 
-        mItemTouchHelper = new ItemTouchHelper(new ItemTouchHelperCallback(mainAdapter));
+        fallingBall = binding.fb;
+        if(isVisible){
+            recyclerView.setOnDragListener(mainAdapter.getDragInstance());
+        }
+        else{
+            recyclerView.setOnDragListener(null);
+        }
+
+        mItemTouchHelper = new ItemTouchHelper(new ItemTouchHelperCallback(mainAdapter, fallingBall, true));
         mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
 
+    private void initAddRecyclerView(){
+        //Add RecyclerView 설정
+        addRecyclerView = binding.rvAddRoutine;
+        addLayoutManager = new LinearLayoutManager(this);
+        addRecyclerView.setLayoutManager(addLayoutManager);
+        addArrayList = new ArrayList<>();
 
+        for(int i=0; i<5; i++){
+            MainData testData = new MainData("Data" +i);
+            addArrayList.add(testData);
+        }
+        addAdapter = new MainAdapter(addArrayList, this);
+        addRecyclerView.setAdapter(addAdapter);
 
+        addRecyclerView.setOnDragListener(addAdapter.getDragInstance());
 
+        addItemTouchHelper = new ItemTouchHelper(new ItemTouchHelperCallback(addAdapter, fallingBall, false));
+        addItemTouchHelper.attachToRecyclerView(addRecyclerView);
     }
 
 
+    //Add RecyclerView show/hide
+    private void showAddList(){
+        addRecyclerView.setVisibility(View.VISIBLE);
+        TranslateAnimation animation = new TranslateAnimation(-addRecyclerView.getWidth(), 0, 0, 0);
+        animation.setDuration(500);
+        addRecyclerView.startAnimation(animation);
+        isVisible = false;
+    }
+    private void hideAddList() {
+        TranslateAnimation animation = new TranslateAnimation(0, -addRecyclerView.getWidth(), 0, 0);
+        animation.setDuration(500);
+        addRecyclerView.startAnimation(animation);
+        addRecyclerView.setVisibility(View.GONE);
+        isVisible = true;
+    }
+
+
+    //List가 비었을 때 Text표시
+    @Override
+    public void setEmptyListTop(boolean visibility) {
+
+    }
+
+    @Override
+    public void setEmptyListBottom(boolean visibility) {
+
+    }
 }
